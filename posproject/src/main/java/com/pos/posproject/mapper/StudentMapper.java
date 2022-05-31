3eb0e92499763e7d4622e001e5237e655910b962
object1997428 <71408968+object1997428@users.noreply.github.com>
2022-05-31 19:20:19 +0900

    mariadb 연결 예제

---
+++ posproject/src/main/java/com/pos/posproject/mapper/StudentMapper.java
@@ -0,0 +1,20 @@
+package com.pos.posproject.mapper;
+
+
+import com.pos.posproject.domain.Student;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface StudentMapper {
+    public List<Student> findList();
+
+    public Student findOneByCode(String codes);
+
+    public void save(Student student);
+
+    public void update(Student student);
+
+    public void deleteById(Integer id);
+}
