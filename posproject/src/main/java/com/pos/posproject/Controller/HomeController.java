13b7ceecbe1d4b5baf28f9043a0b57b5924b9978
wijihye <58647487+wijihye@users.noreply.github.com>
2022-06-06 16:27:36 +0900

    add chatting & tweak controller

--- posproject/src/main/java/com/pos/posproject/Controller/HomeController.java
+++ posproject/src/main/java/com/pos/posproject/Controller/HomeController.java
@@ -1,21 +1,108 @@
 package com.pos.posproject.Controller;
 
+import org.apache.tomcat.jni.Socket;
+import org.json.simple.JSONObject;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
-public class HomeController {
+@ServerEndpoint("/websocket")
+public class  HomeController extends Socket {
+
+    private static final List<Session> session = new ArrayList<Session>();
+    private static final Map<Object, String> user = new HashMap<Object, String>();
+    // session, user_id
+
+    public String make_random_name() {
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        char idx1 = alphabet.charAt((int) (Math.random() * 26));
+        char idx2 = alphabet.charAt((int) (Math.random() * 26));
+        char idx3 = alphabet.charAt((int) (Math.random() * 26));
+
+        String result = Character.toString(idx1) + Character.toString(idx2) + Character.toString(idx3);
+
+        return result;
+    }
+
+    // session 열리면
+    @OnOpen
+    public void open(Session new_user) throws IOException, EncodeException {
+        System.out.println("connected: " + new_user.getId());
+        String new_name = make_random_name();
+
+        user.put(new_user, new_name);
+        session.add(new_user); // 세션에 유저 추가 (내 정보)
+
+        System.out.println("새 유저 추가: " + new_user);
+    }
+
+    // session 닫히면
+    @OnClose
+    public void close(Session close_user) throws IOException {
+        System.out.println(close_user);
+
+        user.remove(close_user);
+        session.remove(close_user); // 세션에서 나간 유저 삭제
+
+        System.out.println("유저 삭제: " + close_user);
+    }
 
 
+    // 메세지 받음
+    @OnMessage
+    public void getMsg(Session recieveSession, String msg) {
+        for (int i = 0; i < session.size(); i++) {
+            if (!recieveSession.equals(session.get(i))) {
+                try {
+                    JSONObject user_info = new JSONObject();
+                    user_info.put("user_name", user.get(recieveSession));
+                    user_info.put("msg", msg);
+                    user_info.put("status",  "other");
 
+                    session.get(i).getBasicRemote().sendText(String.valueOf(user_info));
+                    // 상대
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
+            }else{
+                try {
+                    JSONObject user_info = new JSONObject();
+                    user_info.put("user_name", user.get(session.get(i)));
+                    user_info.put("msg", msg);
+                    user_info.put("status",  "me");
+
+                    session.get(i).getBasicRemote().sendText(String.valueOf(user_info));
+                    // 나 session.get(i)
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
     @GetMapping("/search")
     public String search_group(){
         System.out.println("함수가 되긴함");
         return "find_group";}
+
+    @GetMapping("/search/user-detail")
+    public String user_detail(){
+        return "user_detail";
+    }
 
     @GetMapping("/group")
     public String group_page(){
@@ -25,5 +112,10 @@ public class HomeController {
     @GetMapping("/spring")
     public String springhome(){
         return "springHome.html";
+    }
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
     }
 }
