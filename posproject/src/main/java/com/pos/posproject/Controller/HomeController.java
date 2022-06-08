faace0335d6bee0e7449e28bdf4774cb385b3067
object1997428 <71408968+object1997428@users.noreply.github.com>
2022-06-09 01:14:10 +0900

    exit서비스기능구현/ 로그인,검색 css/ 채팅 아이디 변경/ exit에서 검색버튼클릭시 detail페이지이동 오류발견(해결X)

--- posproject/src/main/java/com/pos/posproject/Controller/HomeController.java
+++ posproject/src/main/java/com/pos/posproject/Controller/HomeController.java
@@ -33,18 +33,25 @@ import java.util.Map;
 @ServerEndpoint("/websocket")
 public class  HomeController extends Socket {
 
+    @Autowired
+    private GroupMapper groupMapper;
+    private Member user1;
+
+    public String userId;
     private static final List<Session> session = new ArrayList<Session>();
     private static final Map<Object, String> user = new HashMap<Object, String>();
     // session, user_id
 
     public String make_random_name() {
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        System.out.println("유저 아이디 만들 때: "+userId);
 
         char idx1 = alphabet.charAt((int) (Math.random() * 26));
         char idx2 = alphabet.charAt((int) (Math.random() * 26));
         char idx3 = alphabet.charAt((int) (Math.random() * 26));
 
         String result = Character.toString(idx1) + Character.toString(idx2) + Character.toString(idx3);
+//        String result=(user1==null)? "123":user1.getId();
 
         return result;
     }
@@ -52,8 +59,10 @@ public class  HomeController extends Socket {
     // session 열리면
     @OnOpen
     public void open(Session new_user) throws IOException, EncodeException {
+        System.out.println("open1 유저아이디: "+userId);
         System.out.println("connected: " + new_user.getId());
         String new_name = make_random_name();
+        System.out.println("open2 유저아이디: "+userId);
 
         user.put(new_user, new_name);
         session.add(new_user); // 세션에 유저 추가 (내 정보)
@@ -78,7 +87,7 @@ public class  HomeController extends Socket {
             if (!recieveSession.equals(session.get(i))) {
                 try {
                     JSONObject user_info = new JSONObject();
-                    user_info.put("user_name", user.get(recieveSession));
+                    user_info.put("user_name", "상대");
                     user_info.put("msg", msg);
                     user_info.put("status",  "other");
 
@@ -91,7 +100,7 @@ public class  HomeController extends Socket {
             }else{
                 try {
                     JSONObject user_info = new JSONObject();
-                    user_info.put("user_name", user.get(session.get(i)));
+                    user_info.put("user_name", "나");
                     user_info.put("msg", msg);
                     user_info.put("status",  "me");
 
@@ -104,10 +113,7 @@ public class  HomeController extends Socket {
         }
     }
 
-    @Autowired
-    private GroupMapper groupMapper;
 
-    private Member user1;
 
     @GetMapping("/css/style.css")
     public String find_style(){
@@ -147,19 +153,6 @@ public class  HomeController extends Socket {
         return "main";
     }
 
-    @GetMapping("/search")
-    public String search_page(Model model){
-        System.out.println("This is a Group Search page.");
-        List<Group> groups =groupMapper.findGroups();
-        model.addAttribute("groups",groups);
-        model.addAttribute("member",user1);
-
-        Time time=new Time(user1.getStudyTime());
-        model.addAttribute("time",time);
-        return "find_group";
-    }
-
-
     @PostMapping("/search")//login을 여기서 해서 search페이지로 넘어감
     public String login(String id,String pwd,Model model){
         Member member=groupMapper.findMemberById(id);
@@ -172,21 +165,47 @@ public class  HomeController extends Socket {
             return "main";
         }
         user1=member;
+        userId=user1.getId();
+        System.out.println("login 유저 아이디: "+userId);
         List<Group> groups =groupMapper.findGroups();
 
         model.addAttribute("member",member);
         model.addAttribute("groups",groups);
 
         Time time=new Time(user1.getStudyTime());
-        System.out.println(time+" 되는데?");
+        model.addAttribute("time",time);
+        return "find_group";
+    }
+
+    @GetMapping("/search")
+    public String search_page(Model model){
+        System.out.println("This is a Group Search page.");
+        System.out.println("search_page 유저 아이디: "+userId);
+
+        List<Group> groups =groupMapper.findGroups();
+        model.addAttribute("groups",groups);
+        model.addAttribute("member",user1);
+
+        Time time=new Time(user1.getStudyTime());
         model.addAttribute("time",time);
         return "find_group";
     }
 
 
+
+
+
     @GetMapping("/search_group")
     public String search_group(@ModelAttribute("title") String title,Model model){
+        System.out.println("검색: 유저 아이디: "+userId);
+
         Group group=groupMapper.findOneByTitle(title);
+        if(group==null){
+            System.out.println("없는 그룹을 검색");
+            System.out.println("model");
+            return search_page(model);
+        }
+
         model.addAttribute("group",group);
         model.addAttribute("member",user1);
         System.out.println("타이틀: "+group.getGroup_title());
@@ -203,6 +222,8 @@ public class  HomeController extends Socket {
     @GetMapping("/search/user-detail")
     public String user_detail(Model model, @RequestParam(value="code", defaultValue = "1") Integer codeNum){
         System.out.println("This is an user detail page.");
+        System.out.println("detail: 유저 아이디: "+userId);
+
         Group group=groupMapper.findOneByCode(codeNum);
         model.addAttribute("group",group);
         model.addAttribute("member",user1);
@@ -216,21 +237,13 @@ public class  HomeController extends Socket {
 
 
     @PostMapping("/exit")
-    public String Exit(Model model, String time){
+    public String Exit(Model model, String time){//공부한 시간 저장
         Integer t=Integer.parseInt(time);
         t+= user1.getStudyTime();
         user1.setStudyTime(t);
         groupMapper.updatestudyTime(user1);
 
-        Time tt=new Time(user1.getStudyTime());
-        model.addAttribute("time",tt);
-
-        //여기서 공부한 시간 받아서 저장해야함
-        System.out.println("This is a Group Search page.");
-        List<Group> groups =groupMapper.findGroups();
-        model.addAttribute("groups",groups);
-        model.addAttribute("member",user1);
-        return "find_group";
+        return search_page(model);
     }
 
     @Bean
--- posproject/src/main/resources/static/js/user_screen_detail.js
+++ posproject/src/main/resources/static/js/user_screen_detail.js
@@ -25,9 +25,28 @@ function showClock() {
     Target.innerText = msg;
 }
 
+
 var timer = setInterval(showClock, 1000);
 
-End.addEventListener('click', () => {
+End.addEventListener("click", () => {
+    console.log("exit함수 시작\n");
+    let form=document.createElement('form')
+
+    let tt=String(hours*360+minutes*60+seconds);
+    console.log(tt);
+    let time=document.createElement('input');
+    time.setAttribute('type','hidden');
+    time.setAttribute('name','time');
+    time.setAttribute('value',tt);
+
+    form.appendChild(time);
+    form.setAttribute('method','post');
+    // form.setAttribute('target','_blank');
+    form.setAttribute('action', '/exit');
+    document.body.appendChild(form);
+
     clearInterval(timer);
     console.log(hours + ':' + minutes + ':' + seconds);
+
+    form.submit();
 });
